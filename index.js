b6e83f2282777a8433b5c91cd8ffcf56298b2a3f
sahinurakter <sahinurakter866@gmail.com>
2025-08-28 15:50:51 +0600

    this is dom part added this part

--- index.js
+++ index.js
@@ -64,7 +64,15 @@ document.getElementById("call-nen").addEventListener("click", function(){
     document.getElementById("coin-amound").innerText = totalcoin
     }
     else{ alert("You donot have enough coins. To make a call, at least 20 coins are required.")}
-
+    let now = new Date()
+let corretTime = now.toLocaleTimeString();
+console.log(corretTime)
+// document.getElementById("add-msg").innerText = corretTime
+const addmsg = document.getElementById("add-msg")
+const addTime =document.getElementById("add-time")
+let newText = "Emarjancy Number"
+addmsg.textContent += newText ;
+addTime.textContent += corretTime
 })
 document.getElementById("call-phn").addEventListener("click", function(){
         const coinAmound = parseInt(document.getElementById("coin-amound").innerText)
@@ -162,60 +170,70 @@ document.getElementById("nen-copy").addEventListener("click" , function(){
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+    let text = 999
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("phn-copy").addEventListener("click" , function(){
     alert("The number has been copied 999")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+ let text = 999
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("fsn-copy").addEventListener("click" , function(){
     alert("The number has been copied 999")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+ let text = 999
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("as-copy").addEventListener("click" , function(){
 alert("The number has been copied 1994-999999")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+ let text = 1994-999999;
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("wch-copy").addEventListener("click" , function(){
     alert("The number has been copied 109")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+ let text = 109;
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("ach-copy").addEventListener("click" , function(){
     alert("The number has been copied 106")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+ let text = 106;
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("eh-copy").addEventListener("click" , function(){
     alert("The number has been copied 16316")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+ let text = 16316;
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("bh-copy").addEventListener("click" , function(){
     alert("The number has been copied 16445")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
-
+ let text = 16445;
+      navigator.clipboard.writeText(text);
 })
 document.getElementById("brh-copy").addEventListener("click" , function(){
     alert("The number has been copied 163")
     const copyAmound = parseInt(document.getElementById("copy-count").innerText)
     const totalcopy = copyAmound + 1
     document.getElementById("copy-count").innerText = totalcopy
+     let text = 163;
+      navigator.clipboard.writeText(text);
 })
